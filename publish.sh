dae4e8a4fcef88d4dcec0bf10ad4859d000c542f
Joshua Tauberer <tauberer@govtrack.us>
2016-08-13 00:41:51 +0000

    split the build so that the parts that require access to our private archive are separate

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 source aws_credentials.txt
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-s3cmd sync --delete-removed -r -P --guess-mime-type --reduced-redundancy build/ s3://crs-reports-website-dev-jt/
+s3cmd sync --delete-removed -r -P --guess-mime-type -F --reduced-redundancy build/ s3://crs-reports-website-dev-jt/
 
