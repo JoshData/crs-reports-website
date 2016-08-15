3161c6214031b9c1e95b458cdb81bc0b203208e3
Joshua Tauberer <tauberer@govtrack.us>
2016-08-15 12:52:10 +0000

    move the S3 bucket names to the credentials file

--- fetch_reports_files.sh
+++ fetch_reports_files.sh
@@ -2,4 +2,4 @@
 source aws_credentials.txt
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-s3cmd sync -s --no-preserve s3://crs-reports-201603/ incoming/
+s3cmd sync -s --no-preserve s3://$AWS_INCOMING_S3_BUCKET/ incoming/
