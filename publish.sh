d6956546d3450d598d221ad8a4cabcd2be450343
Joshua Tauberer <tauberer@govtrack.us>
2016-08-19 14:28:00 +0000

    Revert "create a CloudFront invalidation request after publishing" This can be done better by setting a custom cache default TTL to something like four hours. This reverts commit 5db89108aa3abe5ddff86b2211c19bf7f5220a33.

--- publish.sh
+++ publish.sh
@@ -3,13 +3,9 @@ source aws_credentials.txt
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 
-# Upload to S3.
+# Upload changed files to S3.
 s3cmd sync --delete-removed -r -P --guess-mime-type -F --reduced-redundancy build/ s3://$AWS_WEBSITE_S3_BUCKET/
 
 # MIME-type guessing on the CSS file is broken, so fix the MIME type.
 s3cmd modify s3://$AWS_WEBSITE_S3_BUCKET/static/main.css -m text/css
-
-# Send an invalidation request to CloudFront so that it pulls new content from S3.
-aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id $AWS_DISTRIBUTION_ID --paths "/*"
 
