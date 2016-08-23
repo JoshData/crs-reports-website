d095b46610362f186dec61dcf27607043460d6d7
Joshua Tauberer <tauberer@govtrack.us>
2016-08-23 15:44:39 +0000

    remove -P (public ACLs) flag from s3cmd since we assume the site is published through CloudFront

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 
 # Upload changed files to S3.
-s3cmd sync --delete-removed -r -P --guess-mime-type -F --reduced-redundancy build/ s3://$AWS_WEBSITE_S3_BUCKET/
+s3cmd sync --delete-removed -r --guess-mime-type -F --reduced-redundancy build/ s3://$AWS_WEBSITE_S3_BUCKET/
 
 # MIME-type guessing on the CSS file is broken, so fix the MIME type.
 s3cmd modify s3://$AWS_WEBSITE_S3_BUCKET/static/main.css -m text/css
