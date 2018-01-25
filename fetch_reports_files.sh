7a04415ebf0642282a1b5413b2549b4b5040d1e2
Joshua Tauberer <jt@occams.info>
2018-01-25 03:13:12 +0000

    when pulling down report files, speed it up by not checking hashes

--- fetch_reports_files.sh
+++ fetch_reports_files.sh
@@ -2,4 +2,8 @@
 source credentials.txt
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-s3cmd sync -s --no-preserve s3://$AWS_INCOMING_S3_BUCKET/ incoming/
+s3cmd sync -s --no-preserve --no-check-md5 s3://$AWS_INCOMING_S3_BUCKET/ incoming/
+  # we have one very large file and many other files,
+  # and we're not worried about files being updated in place,
+  # so don't check file hashes - just download if files are new
+  # or size changed
